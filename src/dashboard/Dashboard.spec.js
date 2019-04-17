4a611ccd576e1d095eba155f2281d2fe127b89d2
jmendez44 <jenuel.mendez@gmail.com>
2019-04-17 18:58:12 -0400

    init commit

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from "react";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import Dashboard from "./Dashboard";
+
+describe("Dashboard component tests", () => {
+  it("should match snapshot", () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
